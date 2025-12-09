6d3e9abf0153ca8281425c0b4ee13f6f8892af23
Kolade <105285268+Corelade@users.noreply.github.com>
2025-12-09 22:40:55 +0000

    created-random-function

--- librarybookingsystem/library.py
+++ librarybookingsystem/library.py
@@ -386,6 +386,10 @@ def main():
         pprint(lib.info())
 
 
+def random_function():
+    print('do_nothing')
+
+
 if __name__ == "__main__":
     main()
     # TODO You can't edit the quantity of a book that has been borrowed
