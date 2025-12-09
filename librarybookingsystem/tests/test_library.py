e2d10b2dcb71cf3936919cf155df5191e4d16b54
Kolade <105285268+Corelade@users.noreply.github.com>
2025-12-09 23:43:02 +0000

    should fail

--- librarybookingsystem/tests/test_library.py
+++ librarybookingsystem/tests/test_library.py
@@ -109,7 +109,7 @@ class TestLibrarySystem(BaseTest):
 
     def test_book_not_valid_error(self):
         "Book not valid for borrow book function"
-        with pytest.raises(TypeError, match="Not a Valid Book"):
+        with pytest.raises(TypeError, match="Not a Valid Books"):
             self.lib.borrow_book(person=self.p1, book="Invalid Books", number=2)
 
     def test_book_not_found_error(self):
