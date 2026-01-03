0e35b0a984a682ddfc8da6cdf91e474f8dbd6269
Kolade <105285268+Corelade@users.noreply.github.com>
2026-01-03 10:42:17 +0000

    created a v2 for subarray

--- problem_solving/subarray.py
+++ problem_solving/subarray.py
@@ -14,17 +14,24 @@ def max_subarray(arr: list) -> int:
     return max_sub
 
 
-# def max_subarray_v2(arr: list) -> int:
-    TODO
+def max_subarray_v2(arr: list) -> int:
+    current_max = arr[0]
+    longest_max = arr[0]
+    
+    for i in arr[1:]:
+        current_max = max(i, current_max  + i)
+        longest_max = max(longest_max, current_max)
+        
+    return longest_max
 
 
-print(max_subarray([1, -3, 2, 1, -1]))
-print(max_subarray([4, -1, 2, 1]))
-print(max_subarray([1]))
-print(max_subarray([-2, -3, -1, -5]))
-print(max_subarray([0, 0, 0]))
-print(max_subarray([0, 2, 0]))
-print(max_subarray([-1, 0, -2]))
+print(max_subarray([1, -3, 2, 1, -1]), max_subarray_v2([1, -3, 2, 1, -1]))
+print(max_subarray([4, -1, 2, 1]), max_subarray_v2([4, -1, 2, 1]))
+print(max_subarray([1]), max_subarray_v2([1]))
+print(max_subarray([-2, -3, -1, -5]), max_subarray_v2([-2, -3, -1, -5]))
+print(max_subarray([0, 0, 0]), max_subarray_v2([0, 0, 0]))
+print(max_subarray([0, 2, 0]), max_subarray_v2([0, 2, 0]))
+print(max_subarray([-1, 0, -2]), max_subarray_v2([-1, 0, -2]))
 """
 M([1]) -> 1
 M([1, 2]) -> [1], [1, 2]
